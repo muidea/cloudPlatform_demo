11c1a1912bdc38a3e5c12b3e2cc657d1c0769524
rangh <rangh@ranghs-MacBook.local>
2016-12-19 17:34:56 +0800

    更新HisServer启动脚本

--- startHisServer.sh
+++ startHisServer.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 
-./HisServer -Rabbitmq="amqp://guest:guest@localhost:5672/"
+./HisServer -Rabbitmq="amqp://guest:guest@localhost:5672/" -DataBaseSvr="127.0.0.1" -DataBaseName="cloudPlatform-mongodb"
 
 
