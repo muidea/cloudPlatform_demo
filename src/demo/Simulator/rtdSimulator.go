3ef59059eb6d803779a90110db0cfc5d0bf67719
rangh <rangh@ranghs-MacBook.local>
2016-12-22 17:53:17 +0800

    更新simulator，支持方波，随机数和常值

--- src/demo/Simulator/rtdSimulator.go
+++ src/demo/Simulator/rtdSimulator.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/rpc"
 	"time"
 )
@@ -14,15 +15,23 @@ type Simulator interface {
 	Value() float32
 }
 
-const constLoop = 60
+type constImpl struct {
+	curValue float32
+}
 
-type impl struct {
+func (s *constImpl) Value() float32 {
+	return 50.0
+}
+
+type triangleImpl struct {
 	curValue  float32
 	tickValue int
 	forward   bool
 }
 
-func (s *impl) Value() float32 {
+func (s *triangleImpl) Value() float32 {
+	const constLoop = 60
+
 	curValue := s.curValue
 
 	const step = 1.0
@@ -46,16 +55,85 @@ func (s *impl) Value() float32 {
 	return curValue
 }
 
-// NewSimulator 新建仿真器
-func NewSimulator() Simulator {
-	i := impl{curValue: 20.0, tickValue: 0, forward: true}
-	return &i
+type squareImpl struct {
+	curValue  float32
+	tickValue int
+	forward   bool
 }
 
-func simuValue(client *rpc.Client, simulatorName string, pointName []string) {
+func (s *squareImpl) Value() float32 {
+	const highValue = 70.0
+	const lowValue = 30.0
+	const constScop = 20
+
+	curValue := s.curValue
+
+	s.tickValue++
+
+	if s.forward {
+		s.curValue = highValue
+	} else {
+		s.curValue = lowValue
+	}
+
+	if s.tickValue%constScop == 0 {
+		if s.forward {
+			s.forward = false
+		} else {
+			s.forward = true
+		}
+		s.tickValue = 0
+	}
+
+	return curValue
+}
+
+type randomImpl struct {
+	curValue  float32
+	tickValue int
+	forward   bool
+}
+
+func (s *randomImpl) randFloat(min, max float32) float32 {
+	if max-min <= 0 {
+		return min
+	}
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	return min + float32(rand.Intn(int(max-min)))
+}
+
+func (s *randomImpl) Value() float32 {
+	const highValue = 80.0
+	const lowValue = 20.0
+
+	curValue := s.randFloat(lowValue, highValue)
+
+	return curValue
+}
+
+// NewSimulator 新建仿真器
+func NewSimulator(dataType int) Simulator {
+	switch dataType {
+	case 0:
+		i := triangleImpl{curValue: 20.0, tickValue: 0, forward: true}
+		return &i
+	case 1:
+		i := squareImpl{curValue: 20.0, tickValue: 0, forward: true}
+		return &i
+	case 2:
+		i := randomImpl{curValue: 20.0, tickValue: 0, forward: true}
+		return &i
+	default:
+		i := constImpl{}
+		return &i
+	}
+}
+
+func simuValue(client *rpc.Client, simulatorName string, dataType int, pointName []string) {
 	point2Simulator := map[string]Simulator{}
 	for _, point := range pointName {
-		point2Simulator[point] = NewSimulator()
+		point2Simulator[point] = NewSimulator(dataType)
 	}
 
 	preTimeStamp := time.Now().UTC().UnixNano() / int64(time.Millisecond)
@@ -99,11 +177,13 @@ func main() {
 	var svrPort = 12340
 	var simuName = ""
 	var simueNum = 100
+	var dataType = 0
 	var offSet = 100
 	flag.IntVar(&svrPort, "Port", 0, "rtdService port")
 	flag.StringVar(&svrIP, "IP", "", "rtdService ip")
 	flag.StringVar(&simuName, "Name", "testSimulator", "simulator's name")
 	flag.IntVar(&simueNum, "Number", 0, "point number")
+	flag.IntVar(&dataType, "DataType", 0, "simulate data type, 0:triangle, 1:square, 2:random, other: 50.0")
 	flag.IntVar(&offSet, "Offset", 0, "point name index offset")
 	flag.Parse()
 	if len(svrIP) == 0 || svrPort == 0 {
@@ -134,7 +214,7 @@ func main() {
 
 	log.Printf("construct point ok, size:%d", len(pointName))
 
-	go simuValue(client, simuName, pointName)
+	go simuValue(client, simuName, dataType, pointName)
 
 	log.Printf(" [*] To exit press CTRL+C")
 	<-forever
